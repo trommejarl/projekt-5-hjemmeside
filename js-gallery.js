7b070f9aebd7601322a3a9be78b473e6458e0917
trommejarl <ejkm38523@edu.ucl.dk>
2022-04-15 00:54:43 +0200

    f

---
+++ js-gallery.js
@@ -0,0 +1,17 @@
+let imgArrayA = [
+      'pictures/4.jpg',
+      'pictures/5.jpg',
+      'pictures/1.jpg',
+      'pictures/6.jpg',
+      'pictures/7.jpg'
+  ];
+  let curIndexA = 0;
+  let imgDurationA = 7000;
+
+  function slideShowA() {
+      document.getElementById('pf1').src = imgArrayA[curIndexA];
+      curIndexA++;
+      if (curIndexA == imgArrayA.length) { curIndexA = 0; }
+      setTimeout("slideShowA()", imgDurationA);
+  }
+  slideShowA();
