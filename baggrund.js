d581e4c0af5afae1248f9c35c0e3515022a5bcf1
trommejarl <ejkm38523@edu.ucl.dk>
2022-04-21 18:13:43 +0200

    forside helt done

--- baggrund.js
+++ baggrund.js
@@ -1,11 +1,23 @@
-console.log("bagg")
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollFunction(); scrollFunctionTop()};
 function scrollFunction() {
 if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-
   document.getElementById("bagg").style.background = "#4B7652";
 } else {
-
   document.getElementById("bagg").style.background = "none";
 }
+}
+
+let mybutton = document.getElementById("toppen");
+
+
+function scrollFunctionTop() {
+  if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+for (let i = 0; i < 5; i++){
+  console.log("Dette er et loop", i);
 }
