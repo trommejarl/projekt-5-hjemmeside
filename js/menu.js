83c2f4bbf1984a1dcd79dc4c85addcf43cf266a6
SjelaThrier <sjela-town@hotmail.com>
2022-04-24 16:26:27 +0200

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,15 @@
 /* menubar js */
+/* Acess - hvilke elementer skal aktiveres */
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
+/* animering af x i menu + udfold af menu punkter */
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 })
 
+/* ved klik på menu punkt - folde burger og menu sammen */
 document.querySelectorAll(".nav-link").forEach(n => n.
     addEventListener("click", () => {
         hamburger.classList.remove("active");
